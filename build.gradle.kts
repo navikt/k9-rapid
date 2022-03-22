5752ec9549815af2db7bef9c25fa4df8b5f16e48
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-03-22 14:06:34 +0100

    Gradle wrapper + dep bumps (jackson+RnR) (#22)
    * Gradle + Dep bumps (jackson+RnR)
    
    * Fler checkout bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junitJupiterVersion = "5.8.2"
-val jacksonVersion = "2.13.1"
+val jacksonVersion = "2.13.2"
 val ulidVersion = "8.3.0"
 
 plugins {
