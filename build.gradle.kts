e21a27cdb5c6f4fb62473f3b46c7e639afd3da59
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-09-23 14:46:10 +0200

    Dependency bumps (#37)
    Fjernet bit i kafka-readme om deprecated on-prem kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val junitJupiterVersion = "5.9.0"
-val jacksonVersion = "2.13.3"
+val junitJupiterVersion = "5.9.1"
+val jacksonVersion = "2.13.4"
 val ulidVersion = "8.3.0"
 
 plugins {
