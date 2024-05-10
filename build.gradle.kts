feb6966edd41cb0391e49619eefc9c2439a29f8f
Qadeer Khan <ahm.qadeer@gmail.com>
2024-05-10 10:28:53 +0200

    legger til github registry for rapids and rivers (#118)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,14 @@ subprojects {
 
     repositories {
         mavenCentral()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/navikt/rapids-and-rivers")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: "x-access-token"
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 
     dependencies {
