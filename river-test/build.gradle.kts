cc271018552a127fde0b52eec888ca00d3c81da7
Helga.Nyrud <helga.nyrud@nav.no>
2025-02-25 15:54:00 +0100

    lager river-test modul fordi rapids and rivers lib har flyttet test ut i egne libs. Vi ønsker å ha én versjonsnr på k9-rapid å forholde oss til og derfor finnes det bare versjonsnr i river-test modulen.

---
+++ river-test/build.gradle.kts
@@ -0,0 +1,59 @@
+val tbdLibsRapidsAndRiversTestVersion = "2025.02.25-12.50-0ab5a4f3"
+
+
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+dependencies {
+    api("com.github.navikt.tbd-libs:rapids-and-rivers-test:$tbdLibsRapidsAndRiversTestVersion")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+
+            url = uri("https://maven.pkg.github.com/navikt/k9-rapid")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            pom {
+                name.set("river-test")
+                description.set("River test verktøy som wrapper tbd test support")
+                url.set("https://github.com/navikt/k9-rapid")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/k9-rapid.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/k9-rapid.git")
+                    url.set("https://github.com/navikt/k9-rapid")
+                }
+            }
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "k9-rapid"
-include("behov","losning","river")
+include("behov","losning","river", "river-test")
 include("overfore-omsorgsdager")
 include("overfore-korona-omsorgsdager")
 include("fordele-omsorgsdager")
